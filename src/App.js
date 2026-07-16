b62f8e478fa7e1c2f0c54ec4374e9ad61139b5e0
unknown <devashi7112011@gmail.com>
2026-07-16 22:11:06 +0530

    Basic routing with error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import { createBrowserRouter, Outlet } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 
 // Main Component
 
@@ -9,10 +14,33 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
-      {/* <Footer /> */}
+      < Outlet />
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
+// The syntactical way for rendering the component in React 18 and above.
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { LOGO_URL } from "../../utils/constants.js";
 import {useState} from "react";
+import { Link } from "react-router"
 
 // Header Component
 const Header = () => {
@@ -15,9 +16,9 @@ const Header = () => {
         </div>
         <div className="nav-items">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><Link to="/" >Home</Link></li>
+            <li><Link to= "/about">About </Link></li>
+            <li> <Link to= "/contact">Contact</Link></li>
             <li>Cart</li>
             <button className="login-btn"
             onClick={() => {
