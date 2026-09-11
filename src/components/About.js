69c51a33e755dfa58d72682b22e687bcdc861caa
unknown <devashi7112011@gmail.com>
2026-09-11 13:45:18 +0530

    feat: class based component basic structure

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import UserClass from './UserClass.js';
+import {Component} from 'react';
 
-const About = () => {
-    return (
-        <div>
-            <h1>About Page</h1>
-            <p>This is the about page.</p>
-            <UserClass name={"Devendra Jha"} location={"Hyderabad, India"} />
-        </div>
-    );
+class About extends Component {
+    constructor(props) {
+        super(props);
+        //console.log("Parent Constructor");
+    }
+    componentDidMount() {
+        //console.log("Parent Component Did Mount");
+    }
+    render() {
+        return (
+            <div>
+                <h1>About Page</h1>
+                <p>This is the about page.</p>
+                <UserClass name={"Devendra Jha"} location={"Hyderabad, India"} />
+            </div>
+        );
+    }
 }
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,16 +3,36 @@ import react from 'react';
 class UserClass extends react.Component {
     constructor(props) {
         super(props);
-        console.log("UserClass constructor called", props)
-    }
+        
+        this.state={
+            userInfo:{
+                name: "John Doe",
+                location: "Hyderabad, India",
+                company: "Google"
+            }
+        }
 
+        // console.log("child constructor", props)
+    }
+    async componentDidMount() {
+        const userData = await fetch('https://api.github.com/users/devendrajha-software-developer');
+        const data = await userData.json();
+        console.log( data);
+        this.setState({
+            userInfo: data
+        })
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
     render(){
-        const { name, location } = this.props;
+        const { name, location,company } = this.state.userInfo;
         return(
             <div className="user-class-card">
                 <h2>User Class Component</h2>
                 <p>Name: {name}</p>
                 <p>Location: {location}</p>
+                <p>Company: {company}</p>
             </div>
         );  
     }
